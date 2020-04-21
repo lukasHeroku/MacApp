305abbaef1914f4363a50b4289579305734c108f
Jonathan Krug <jonathankrug@gmx.de>
2020-04-21 10:01:09 +0200

    wrote unit tests

--- MacAppTests/MacAppTests.swift
+++ MacAppTests/MacAppTests.swift
@@ -10,25 +10,21 @@ import XCTest
 @testable import MacApp
 
 class MacAppTests: XCTestCase {
+    
+    var sut: Int!
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    override func setUp() {
+        super.setUp()
+        sut = 9
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testIfSUTIsNine() {
+        XCTAssertEqual(sut, 9, "SUT is not 9!")
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
     }
 
 }
